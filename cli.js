12e493cbff59463bcacd71c4c0031c356c63cd6a
Enivia <chuchu.qing@iwellmass.com>
2022-02-23 16:21:51 +0800

    feat: add qrcode method

--- cli.js
+++ cli.js
@@ -3,17 +3,23 @@
 const { Command } = require("commander");
 
 const search = require("./lib/search");
-
+const qrcode = require("./lib/qrcode");
 const program = new Command();
-console.log("hello enivia");
 
+// search
 program
   .command("search <keyword>")
-  .description("quick search")
+  .description("Quick search")
   .option("-n, --npm", "search by npm")
   .option("-s, --stackoverflow", "search by stackoverflow")
   .option("-g, --github", "search by github")
   .option("-d, --default", "search by google")
   .action((keyword, options) => search(keyword, options));
+
+// qrcode
+program
+  .command("qrcode <content>")
+  .description("Generate qrcode")
+  .action((content) => qrcode(content));
 
 program.parse();
