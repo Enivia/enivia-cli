1dd9f7615cf6ebdb014a64c1d985f4e65bd81f5d
Enivia <chuchu.qing@iwellmass.com>
2022-02-24 17:36:59 +0800

    feat: add uuid and server

--- cli.js
+++ cli.js
@@ -4,6 +4,8 @@ const { Command } = require("commander");
 
 const search = require("./lib/search");
 const qrcode = require("./lib/qrcode");
+const uuid = require("./lib/uuid");
+const server = require("./lib/server");
 const program = new Command();
 
 // search
@@ -21,5 +23,20 @@ program
   .command("qrcode <content>")
   .description("Generate qrcode")
   .action((content) => qrcode(content));
+
+// uuid
+program
+  .command("uuid")
+  .description("Generate id")
+  .option("-p, --pure", "id only includes number and string")
+  .action((options) => uuid(options));
+
+// server
+program
+  .command("server")
+  .description("Start local server")
+  .option("-d, --dist", "serves static directory")
+  .option("-p, --port", "listens for connections on the specified port")
+  .action((options) => server(options));
 
 program.parse();
